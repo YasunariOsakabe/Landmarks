bf9ea4e57652abe98c5e1f3f19780027c19c6f11
Yasunari Osakabe <yasunari.osakabe@init-inc.com>
2024-07-20 17:12:38 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 小坂部泰成 on 2024/07/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
