0456351d6ccb4543cce3812048286231e637e55f
Yasunari Osakabe <yasunari.osakabe@init-inc.com>
2024-07-27 10:44:37 +0900

    LandmarkList.swiftを作成

---
+++ Landmarks/View/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 小坂部泰成 on 2024/07/27.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
