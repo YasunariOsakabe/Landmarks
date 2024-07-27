10f21f2355b0fceb905399e2ea4fb050982e4d86
Yasunari Osakabe <yasunari.osakabe@init-inc.com>
2024-07-27 09:32:07 +0900

    Landmarkモデルクラスの画像,位置情報を管理するプロパティを定義

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation //MapKitとのやりとりに役立つ
 
 //Hashable - ハッシュ値の使用を可能にするプロトコル
 struct Landmark: Codable, Hashable {
@@ -14,4 +16,25 @@ struct Landmark: Codable, Hashable {
     var park: String
     var state: String
     var description: String
+    
+    //Assetsから画像を読み込む計算型プロパティを定義
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    //ランドマークの場所に関する情報を管理
+    //coordinates - 座標
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double //緯度
+        var longitude: Double //経度
+    }
 }
