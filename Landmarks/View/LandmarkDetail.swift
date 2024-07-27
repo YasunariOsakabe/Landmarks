705b2c1f8cb8332337c190b7b5fa4c5d184372ad
Yasunari Osakabe <yasunari.osakabe@init-inc.com>
2024-07-27 15:07:29 +0900

    LandmarkDetail.swiftを定義

---
+++ Landmarks/View/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by 小坂部泰成 on 2024/07/27.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
