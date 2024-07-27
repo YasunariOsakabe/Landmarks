ebd0d17f5ab3548ae22829e5a50c7df02514f004
Yasunari Osakabe <yasunari.osakabe@init-inc.com>
2024-07-27 15:08:24 +0900

    NavigationLinkで詳細画面へ遷移するロジックを定義

--- Landmarks/View/ContentView.swift
+++ Landmarks/View/ContentView.swift
@@ -9,39 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            CircleImage()
-                .offset(y: -130) //y軸方向(垂直)に-130を指定 →130pt上に移動
-                .padding(.bottom, -130) //下の余白を130pt縮小
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                VStack(alignment: .leading) {
-                    Text("Turtle Rock")
-                        .font(.title)
-                    .foregroundColor(.black)
-                    HStack {
-                        Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
-                        Spacer() //Spacerを入れるとTextが画面の左右両端に揃うようにスペースが入る
-                        Text("California")
-                    }
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
-                    
-                    Divider() //区切り線を表示
-                    
-                    Text("About Turtle Rock")
-                        .font(.title2)
-                    Text("Descriptive text goes here.")
-                }
-            }
-            .padding()
-            
-            Spacer() //上下両端いっぱいにView設定をするためにVStackに対してSpacerを設定
-        }
+        LandmarkList()
     }
 }
 
